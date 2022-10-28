57c130894ec89eadda2f20a60c56ae709b2778d5
zhang <zhang@163.com>
2022-10-28 11:59:39 +0800

    test

--- java8/src/test/java/com/example/jdk18demo/AscComparator.java
+++ java8/src/test/java/com/example/jdk18demo/AscComparator.java
@@ -7,6 +7,7 @@ public class AscComparator implements Comparator<Cat> {
     @Override
     public int compare(Cat stu1, Cat stu2) {
 
+
         // 根据成绩降序排列
         return stu1.getRecord() - stu2.getRecord();
     }
